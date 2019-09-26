a19304881d423598d5cf6eda95306f3440f11954
jyoh2k <jyoh2k@gmail.com>
2019-09-26 15:03:37 +0900

    20190926 15:03

--- src/main/main.go
+++ src/main/main.go
@@ -2,60 +2,25 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"net"
+	"os"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello, Card world!")
-
-	const num int = 20
-	var card = [num]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	const nPlayer int = 2
-	var user [nPlayer][2]int
-	var result [nPlayer]int
-
-	// user := make([][]int, nPlayer, 2)
-
-	// for i := 0; i < num; i++ {
-	// 	card = append(card, rand.Intn(10)+1)
-	// }
-
-	for i := 0; i < nPlayer; i++ {
-		for {
-			a := rand.Intn(num)
-			// fmt.Println(a)
-			if card[a] != 0 {
-				user[i][0] = card[a]
-				card[a] = 0
-				break
-			}
-		}
-		for {
-			b := rand.Intn(num)
-			if card[b] != 0 {
-				user[i][1] = card[b]
-				card[b] = 0
-				break
-			}
-		}
+	connect, err := net.Dial("tcp", "127.0.0.1:5555")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	fmt.Println(card)
-	fmt.Println(user)
-
-	for i := 0; i < nPlayer; i++ {
-		result[i] = (user[i][0] + user[i][1]) % 10
+	// some event happens
+	i := 0
+	for {
+		// heartbeat
+		connect.Write([]byte("HIHI"))
+		fmt.Println("Send Data : ", "HIHI", i)
+		i++
+		time.Sleep(time.Second * 1)
 	}
-
-	k := 0
-	v := 0
-	for i, val := range result {
-		fmt.Println(i, val)
-		if v < result[i] {
-			k = i
-			v = result[i]
-		}
-	}
-
-	fmt.Printf("Winner %d th, Score : %d", k+1, v)
 }
